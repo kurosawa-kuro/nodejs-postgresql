086db5fb10f28f002ccf75e51d95271bc74884f1
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-09-25 06:09:56 +0000

    add

--- src/selectToDatabase.test.js
+++ src/selectToDatabase.test.js
@@ -1,34 +1,37 @@
-import postgresql from 'pg';
-const { Client } = postgresql;
+// Importing Dependencies
+import { Client } from 'pg';
+import fetchTodos from './selectToDatabase';
 
-import fetchTodos from './selectToDatabase'; // デフォルトエクスポートの場合のインポートの仕方に変更
-
-// テストデータベース接続情報
+// Test Database Configuration
 const testConfig = {
     host: 'localhost',
     user: 'dev_user',
     password: 'dev_password',
-    database: 'test_db'
+    database: 'test_db',
 };
 
 const client = new Client(testConfig);
 
+// Setting up the Test Environment
 beforeAll(async () => {
     await client.connect();
-    // テーブル名を小文字に統一
-    await client.query(`CREATE TABLE IF NOT EXISTS todo (
-        id SERIAL PRIMARY KEY,
-        title VARCHAR(255) NOT NULL
-    )`);
+    await client.query(`
+    CREATE TABLE IF NOT EXISTS todo (
+      id SERIAL PRIMARY KEY,
+      title VARCHAR(255) NOT NULL
+    )
+  `);
+    await client.query('TRUNCATE todo RESTART IDENTITY;');
     await client.query(`INSERT INTO todo (title) VALUES ('Test Todo')`);
 });
 
+// Cleaning up the Test Environment
 afterAll(async () => {
-    // idを含めてtableを空にする
-    await client.query('TRUNCATE TABLE todo');
+    await client.query('TRUNCATE todo RESTART IDENTITY;');
     await client.end();
 });
 
+// Test Suite
 describe('fetchTodos', () => {
     it('should fetch todos from the test database', async () => {
         const todos = await fetchTodos();
